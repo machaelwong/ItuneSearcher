f3981967ebd653951b76aaa33e07fba7d62dad49
machael <machael.hw.wong@gmail.com>
2023-04-19 11:25:34 +0800

    home page implement

---
+++ app/src/main/java/mako/application/itunesearcher/fragment/BaseBindingFragment.kt
@@ -0,0 +1,30 @@
+package mako.application.itunesearcher.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseBindingFragment<T : ViewDataBinding>: Fragment(){
+    lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, getLayoutResources(), container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onViewCreated(binding)
+    }
+
+    abstract fun getLayoutResources(): Int
+    abstract fun onViewCreated(viewBinding: T)
+}
