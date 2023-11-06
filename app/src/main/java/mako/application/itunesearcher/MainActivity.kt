e8c416cdf61a2f6e005b1d572161748339fc4498
729 <729@stlnd.net>
2023-11-06 14:09:16 +0800

    remove not usful line

--- app/src/main/java/mako/application/itunesearcher/MainActivity.kt
+++ app/src/main/java/mako/application/itunesearcher/MainActivity.kt
@@ -22,9 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//         throw RuntimeException("Test Crash") // Force a crash
-
         supportFragmentManager.apply {
             beginTransaction().replace(R.id.home_container, homeFragment).commitAllowingStateLoss()
         }
